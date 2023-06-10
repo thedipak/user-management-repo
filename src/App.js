5d54898abc8dd2723533fe74ea87bd1ae20c59c6
Yadav <yadadip@apac.corpdir.net>
2023-06-10 17:08:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [response, setResponse] = useState("");
+
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const obj = {
+      name,
+      phone,
+      email
+    }
+    console.log(obj);
+
+    axios.get("http://localhost:5000/getUser")
+    .then(res => {
+      console.log("inside then", res);
+      setResponse(res.data.message.email);
+      setName("");
+      setPhone("");
+      setEmail("");
+    })
+    .catch(err => console.log(err));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> User's Directory </h1>
+      <input type="text" value={name} placeholder="name" onChange={e => setName(e.target.value)} />
+      <input type="number" value={phone} placeholder="phone" onChange={e => setPhone(e.target.value)} />
+      <input type="email" value={email} placeholder="email" onChange={e => setEmail(e.target.value)} />
+      <button onClick={handleSubmit}> Submit </button>
+      { response? <h2> {response} </h2> : null}
     </div>
   );
 }
